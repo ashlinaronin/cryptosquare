80cb5af150fd83aa1c05a727f9de3beadb14350b
Ashlin Aronin & Jordan Johansen <student@epicodus.com>
2015-09-08 12:05:29 -0700

    Write special chars spec and make it fail

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,8 @@
 var encode = function(message) {
     var output = "";
 
-    output = message.toLowerCase();
+    // Remove whitespace and convert to lower case
+    output = message.toLowerCase().replace(/\s+/g, "");
 
     return output;
 }
--- spec/specs.js
+++ spec/specs.js
@@ -2,4 +2,12 @@ describe('encode', function() {
     it("makes SANTACLAUS into santaclaus", function() {
         expect(encode("SANTACLAUS")).to.equal("santaclaus");
     });
+
+    it("makes SANTA CLAUS into santaclaus", function() {
+        expect(encode("SANTA CLAUS")).to.equal("santaclaus");
+    });
+
+    it("makes $ANTA CLAUS!! into santaclaus", function() {
+        expect(encode("$ANTA CLAUS!!")).to.equal("antaclaus");
+    });
 });
