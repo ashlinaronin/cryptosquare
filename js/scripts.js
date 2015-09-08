8ad24899ecac7efb6b299ef5f417c17b6aaf62a0
Ashlin Aronin & Jordan Johansen <student@epicodus.com>
2015-09-08 14:56:15 -0700

    Write test for total square slicer and make it pass.

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,31 @@
-var encode = function(message) {
+var format = function(message) {
     var output = "";
 
     // Remove whitespace and convert to lower case
     output = message.toLowerCase().replace(/[^\w]+/gi, "");
 
     return output;
-}
+};
 
 var columnCount = function(message) {
     var output = "";
-
-    var numberOfCharacters = encode(message).length;
-
+    var numberOfCharacters = format(message).length;
     var numberOfColumns = Math.sqrt(numberOfCharacters);
+
     if (numberOfColumns % 1 === 0) {
         return numberOfColumns;
     } else {
-        return false;
-    }
-}
+        return Math.ceil(numberOfColumns);
+    };
+};
+
+var encode = function(message) {
+    var output = [];
+    var formattedMessage = format(message);
+    var numberOfColumns = columnCount(formattedMessage);
+
+    for (var i = 0; i < formattedMessage.length; i += numberOfColumns) {
+        output.push(formattedMessage.slice(i, i + numberOfColumns));
+    };
+    return output;
+};
--- spec/specs.js
+++ spec/specs.js
@@ -1,36 +1,53 @@
-describe('encode', function() {
-    it("makes SANTACLAUS into santaclaus", function() {
-        expect(encode("SANTACLAUS")).to.equal("santaclaus");
+describe('format', function() {
+    it("handles uppercase", function() {
+        expect(format("SANTACLAUS")).to.equal("santaclaus");
     });
 
-    it("makes SANTA CLAUS into santaclaus", function() {
-        expect(encode("SANTA CLAUS")).to.equal("santaclaus");
+    it("handles whitespace", function() {
+        expect(format("SANTA CLAUS")).to.equal("santaclaus");
     });
 
-    it("makes $ANTA CLAUS number 1!! into antaclausnumber1", function() {
-        expect(encode("$ANTA CLAUS number 1!!")).to.equal("antaclausnumber1");
+    it("handles special characters", function() {
+        expect(format("$ANTA CLAUS number 1!!")).to.equal("antaclausnumber1");
     });
 });
 
 describe('columnCount', function() {
-    it("is 8 columns for a 64 character input", function() {
+    it("returns the square root for an input with a whole number square root", function() {
+        // 64 character input -> 8 columns
         var inputString = "don't compare yourself to others, compare yourself person you were yesterday";
-        var flattenedString = encode(inputString);
+        var flattenedString = format(inputString);
 
         expect(columnCount(flattenedString)).to.equal(8);
     });
 
-    it("is 4 columns for a 16 character input", function() {
+    it("returns the square root for an input with a smaller whole number square root", function() {
+        // 16 character input -> 4 columns
         var inputString = "Portlands NachoWk!";
-        var flattenedString = encode(inputString);
+        var flattenedString = format(inputString);
 
         expect(columnCount(flattenedString)).to.equal(4);
     });
 
-    it("is 9 columns for a 69 character input", function() {
+    it("handles a non-whole-number whole root case", function() {
+        // 69 character input -> 9 columns
         var inputString = "don't compare yourself to others, compare yourself to the person you were yesterday";
-        var flattenedString = encode(inputString);
+        var flattenedString = format(inputString);
 
         expect(columnCount(flattenedString)).to.equal(9);
     });
+});
+
+describe('encode', function() {
+    it("creates an array of strings from inputString, each with length equal to number of columns", function() {
+        var inputString = "Portlands NachoWk!";
+        var flattenedString = format(inputString);
+        var rowLength = columnCount(flattenedString);
+        var firstItemLength = encode(flattenedString)[0].length;
+
+        expect(firstItemLength).to.equal(4);
+
+    });
+
+
 });
