6472c775e92863d8e373feba3529d0daf4d84b05
Ashlin Aronin & Jordan Johansen <student@epicodus.com>
2015-09-08 12:19:54 -0700

    Write square size spec and make it fail

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,11 @@ var encode = function(message) {
     var output = "";
 
     // Remove whitespace and convert to lower case
-    output = message.toLowerCase().replace(/\s+/g, "");
+    output = message.toLowerCase().replace(/[^\w]+/gi, "");
 
     return output;
+}
+
+var squareSize = function(message) {
+    
 }
--- spec/specs.js
+++ spec/specs.js
@@ -7,7 +7,16 @@ describe('encode', function() {
         expect(encode("SANTA CLAUS")).to.equal("santaclaus");
     });
 
-    it("makes $ANTA CLAUS!! into santaclaus", function() {
-        expect(encode("$ANTA CLAUS!!")).to.equal("antaclaus");
+    it("makes $ANTA CLAUS number 1!! into antaclausnumber1", function() {
+        expect(encode("$ANTA CLAUS number 1!!")).to.equal("antaclausnumber1");
     });
 });
+
+describe('squareSize', function() {
+    it("is 8 rows by 9 columns for a 69 character input", function() {
+        var inputString = "don't compare yourself to others, compare yourself to the person you were yesterday";
+        var flattenedString = encode(inputString);
+
+        expect(squareSize(flattenedString)).to.equal([8,9]);
+    });
+})
