52bdbce26c0896a80700f4a63581454c086bbf2c
Ashlin Aronin & Jordan Johansen <student@epicodus.com>
2015-09-08 16:46:59 -0700

    Get jQuery display page working

---
+++ js/formprocessing.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+    $("form#cryptosquare").submit(function(event) {
+        var message = $("input#message").val();
+        var encodedMessage = encode(message);
+
+        var textToPrint = "";
+
+        encodedMessage.forEach(function(row) {
+            textToPrint += row + " ";
+        });
+
+        $("#result").text(textToPrint);
+
+
+
+        $("#result").show();
+        event.preventDefault();
+    })
+});
